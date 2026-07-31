cfd9a139cd42a4fff3adedfa9cecf47ef5fb01e2
Daniel Sanchez <danielsanchezibanez176@gmail.com>
2026-07-30 21:19:24 -0500

    Configurar base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,36 +3,18 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/curso_bash/',
+
   plugins: [react()],
+
   resolve: {
     alias: {
-      '@':           resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src'),
       '@components': resolve(__dirname, 'src/components'),
-      '@hooks':      resolve(__dirname, 'src/hooks'),
-      '@data':       resolve(__dirname, 'src/data'),
-      '@styles':     resolve(__dirname, 'src/styles'),
-      '@utils':      resolve(__dirname, 'src/utils'),
+      '@hooks': resolve(__dirname, 'src/hooks'),
+      '@data': resolve(__dirname, 'src/data'),
+      '@styles': resolve(__dirname, 'src/styles'),
+      '@utils': resolve(__dirname, 'src/utils'),
     },
   },
-  build: {
-    outDir:    'dist',
-    sourcemap: false,
-    minify:    'esbuild',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react:     ['react', 'react-dom'],
-          days1to10: ['./src/data/days1to10.js'],
-          days11to20:['./src/data/days11to20.js'],
-          days21to30:['./src/data/days21to30.js'],
-        },
-      },
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-    host: true,
-  },
-  preview: { port: 4173 },
 })
